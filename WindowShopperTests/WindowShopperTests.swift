0276ea1336370480d8c608ef3954377c590ba20b
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2020-05-05 22:28:57 +0530

    Fixed layout issues

--- WindowShopperTests/WindowShopperTests.swift
+++ WindowShopperTests/WindowShopperTests.swift
@@ -33,6 +33,7 @@ class WindowShopperTests: XCTestCase {
     func testGetHours(){
         XCTAssert(Wage.getHours(forWage: 25, andPrice: 100) == 4)
         XCTAssert(Wage.getHours(forWage: 15.50, andPrice: 250.53) == 17)
+        XCTAssert(Wage.getHours(forWage: 26.3, andPrice: 19200) == 731)
     }
 
 }
