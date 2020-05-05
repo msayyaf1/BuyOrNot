b08f4c2e6ef8c2b7096f311e10adb5bbc8cbc2dc
Mohamed Sayyaf <m.sayyaf01@gmail.com>
2020-05-05 17:56:47 +0530

    Interface builder syncing, IBDesignable

--- WindowShopper/Views/CurrencyTxtField.swift
+++ WindowShopper/Views/CurrencyTxtField.swift
@@ -8,46 +8,36 @@
 
 import UIKit
 
+@IBDesignable // Class attrubute for making interface builder sync with changes
 class CurrencyTxtField: UITextField {
+    // custom class for UI Elements Here we created a custom class called currencytextfield subclass of textfield
+    
+    // custom function for interface builder syncing
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
 
-    // custom class for UI Elements
-    // Here we created a custom class called currencytextfield subclass of textfield
-    
-    
     //When app is loaded this code will run
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        backgroundColor = #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 0.2496698944)  //25percent opacity of white
-        layer.cornerRadius = 5.0 //setting the corner radius of the field
-        textAlignment = .center
-        
-        //setting placeholder text
+        customizeView()
+    }
+    
+    func customizeView() {
+           
+           backgroundColor = #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 0.2496698944)  //25percent opacity of white
+           layer.cornerRadius = 5.0 //setting the corner radius of the field
+           textAlignment = .center
+           
+           //setting placeholder text
 
-        if let p = placeholder { //check if placeholder is nil, if it is nil, this codeblock doesnt run
-            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
-             // now place is a attributedstring with a propery of white foreground color (for manipulating strings)
-             
-            attributedPlaceholder = place //overrides the regular placeholder, and uses place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+           if let p = placeholder { //check if placeholder is nil, if it is nil, this codeblock doesnt run
+               let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+                // now place is a attributedstring with a propery of white foreground color (for manipulating strings)
+                
+               attributedPlaceholder = place //overrides the regular placeholder, and uses place
+               textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-        
-        // BEFORE :- REFACTORING
-        
-//        if placeholder == nil {
-//            placeholder = " "
-//        }
-//
-//        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor:       UIColor.white])
-
-        
-// now place is a attributedstring with a propery of white foreground color (for manipulating strings)
-//
-//         attributedPlaceholder = place //overrides the regular placeholder, and uses place
-//         textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-//
-        
-        
     }
     
 }
